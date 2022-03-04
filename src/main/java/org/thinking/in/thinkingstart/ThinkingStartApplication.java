f034f700214729ae1e302fdeabde726f62adf67c
banxuelai001 <banxuelai001@ke.com>
2022-03-04 15:33:33 +0800

    start run

---
+++ src/main/java/org/thinking/in/thinkingstart/ThinkingStartApplication.java
@@ -0,0 +1,13 @@
+package org.thinking.in.thinkingstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThinkingStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThinkingStartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/thinking/in/thinkingstart/ThinkingStartApplicationTests.java
@@ -0,0 +1,13 @@
+package org.thinking.in.thinkingstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThinkingStartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
