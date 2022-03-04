fa0736c9167b10b01b4c4283ec5db4bd014dd2da
banxuelai001 <banxuelai001@ke.com>
2022-03-04 15:44:31 +0800

    start second

--- thinking-start/src/main/java/org/thinking/in/ThinkingStartApplication.java
+++ thinking-start/src/main/java/org/thinking/in/ThinkingStartApplication.java
@@ -1,13 +1,20 @@
-package org.thinking.in.thinkingstart;
+package org.thinking.in;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ThinkingStartApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ThinkingStartApplication.class, args);
     }
 
+    @GetMapping("hello")
+    public String hello() {
+        return "hello";
+    }
 }
